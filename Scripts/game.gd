0a856610f71cc15c961536219a142c3e412f537a
Felipe Amorim <felipeamorim.dev@gmail.com>
2019-04-11 00:29:10 -0300

    Adicionado o sinal para conectar a variável vida ao label

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -4,9 +4,15 @@ const TYPE_PLAYER = "player"
 const TYPE_ENEMY = "enemy"
 const TYPE_PLATFOMER = "platformer_movel"
 var score = 0
+var vida_player = 3 setget set_vidaPlayer
 
+signal carrega_vida
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	pass
+
+func set_vidaPlayer(valor):
+	vida_player = valor
 	pass
